0dba8008f7301801f8e2bc10c9251fd419c38b3e
HaxeFloppa <rafaelandyhailu@gmail.com>
2025-09-26 12:41:28 +0100

    init

---
+++ client.js
@@ -0,0 +1,26 @@
+const net = require(['net']);
+const PORT = 11110;
+const HOSTNAME = '192.168.1.94';
+
+const client = net.createConnection(PORT, HOSTNAME, () => {
+	console.log('Connected to server');
+	// client.write('yo');
+});
+
+function sendreq() {
+	client.write('sending request!!');
+};
+
+client.setEncoding('utf8');
+
+client.on('data', (data) => {
+	console.log(`Received data from server: ${data}`);
+});
+
+client.on('end', () => {
+	console.log('Disconnected from server');
+});
+
+client.on('error', (err) => {
+	console.error('Connection error: ', err);
+});
