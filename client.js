9c78917847267944e28874b0161f1de80316619d
HaxeFloppa <rafaelandyhailu@gmail.com>
2025-09-27 07:45:17 +0100

    encrypted tls

--- client.js
+++ client.js
@@ -1,6 +1,6 @@
 // const socket = new WebSocket("ws://5.80.142.132");
 // const WebSocket = require('ws');
-const socket = new WebSocket("ws://5.80.142.132:11110");
+const socket = new WebSocket("wss://5.80.142.132:11110");
 document.getElementById("reqsend").addEventListener("click", reqsend);
 
 socket.onopen = () => {
